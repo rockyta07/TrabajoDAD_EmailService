763907e2f56a347db16296fd5e7cb630bdda89fd
rockyta07 <arranze.rocio@gmail.com>
2023-03-23 17:19:45 +0100

    Creación del API REST

---
+++ src/main/java/fluffandpaws/webadopcionemailservice/controller/AdoptionController.java
@@ -0,0 +1,30 @@
+package fluffandpaws.webadopcionemailservice.controller;
+
+import fluffandpaws.webadopcionemailservice.model.AdoptionCertificate;
+import fluffandpaws.webadopcionemailservice.service.AdoptionService;
+import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdoptionController {
+
+    /*@Autowired
+    private JavaMailSender mail;*/
+
+    // Para recoger los requests de email
+    public record AdoptionRequest(String to, String subject, String body){
+
+    }
+
+    @Autowired
+    AdoptionService adoptionService;
+
+    @PostMapping("/peticionAdopcion")
+    public void sendResponse (@RequestBody AdoptionRequest request){
+        adoptionService.sendTestMessage(new AdoptionCertificate(request.to, request.subject, request.body));
+    }
+
+}
